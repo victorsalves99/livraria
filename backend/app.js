7ce15d36cf16b926e78acc9f102f9e098aeb3763
victorsalves99 <victorsalves99@outlook.com>
2024-05-05 13:36:02 -0300

    correção de porta

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config()
 
-const port = process.env.PORT || 4000;
+const port = 8080;
 
 const router = require("./routers/bookRoutes");
 
